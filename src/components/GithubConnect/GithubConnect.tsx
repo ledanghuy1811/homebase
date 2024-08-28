3ddb924e9b1b06180424c057772c5b7263094d8f
ledanghuy1811 <huygamo567@gmail.com>
2024-08-28 15:52:35 +0700

    feat: add UI after connected github

--- src/components/GithubConnect/GithubConnect.tsx
+++ src/components/GithubConnect/GithubConnect.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
+
+import styles from './GithubConnect.scss';
 
 import { Button } from 'components/Button';
 import { ReactComponent as GitICon } from 'assets/images/git-logo.svg';
-import styles from './GithubConnect.scss';
+import ConnectedImg from 'assets/images/connected-img.png';
+import DropdownIcon from 'assets/icons/nav-arrow-down.svg';
 
 const GithubConnect: React.FC = () => {
   const cx = classNames.bind(styles);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [credit, setCredit] = useState<number>(152);
 
   return (
     <div className={cx('wrapper')}>
@@ -16,11 +21,35 @@ const GithubConnect: React.FC = () => {
         </Button>
       </div>
 
-      <div>
-        <Button type="third" onClick={() => {}} icon={<GitICon />} style={{ paddingLeft: 14, paddingRight: 14 }}>
-          Connect Github
-        </Button>
-      </div>
+      {isConnected ? (
+        <div className={cx('connected-area')}>
+          <div className={cx('connected-img')}>
+            <img src={ConnectedImg} alt="Connected Img" />
+          </div>
+
+          <div className={cx('connected-content')}>
+            <div className={cx('connected-info')}>
+              <h1 className={cx('connected-info--name')}>hoangla16</h1>
+              <h1 className={cx('connected-info--credit')}>{credit} credits</h1>
+            </div>
+
+            <img src={DropdownIcon} alt="dropdown icon" />
+          </div>
+        </div>
+      ) : (
+        <div className={cx('connected-btn')}>
+          <Button
+            type="third"
+            onClick={() => {
+              setIsConnected(!isConnected);
+            }}
+            icon={<GitICon />}
+            style={{ paddingLeft: 14, paddingRight: 14 }}
+          >
+            Connect Github
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
